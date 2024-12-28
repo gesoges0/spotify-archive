1be915daef53c82667630f01fab0f39ecb20d753
gesoges0 <elasticnet1995@gmail.com>
2024-12-28 22:04:24 +0900

    fix

--- src/models.py
+++ src/models.py
@@ -22,6 +22,7 @@ sp = spotipy.Spotify(
         client_secret=os.environ["CLIENT_SECRET"],
         redirect_uri="http://localhost:5000/callback",
         scope="playlist-modify-public playlist-modify-private playlist-read-private",
+        cache_path=os.environ["SPOTIFY_CACHE"],
     )
 )
 
